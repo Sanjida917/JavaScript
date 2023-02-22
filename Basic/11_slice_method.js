8963056750a1a4998252568134147018b34a746e
Sanjida Rahman <105595349+Sanjida917@users.noreply.github.com>
2023-02-23 00:57:07 +0600

    Add files via upload

---
+++ Basic/11_slice_method.js
@@ -0,0 +1,12 @@
+
+// start index 
+// end index
+
+let firstName = "Sanjida Papon";
+
+//the string from start index(0) to end index(7-1=6)
+let newString = firstName.slice(0,7);  
+console.log(newString);
+
+newString = firstName.slice(1);  //starts from the 1st index to all
+console.log(newString);
