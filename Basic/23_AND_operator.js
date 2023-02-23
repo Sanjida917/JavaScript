290545350dce82c3257a6f33cb242bf05d2af640
Sanjida Rahman <105595349+Sanjida917@users.noreply.github.com>
2023-02-23 18:05:31 +0600

    Add files via upload

---
+++ Basic/23_AND_operator.js
@@ -0,0 +1,24 @@
+
+
+let name = "Sanjida";
+let age = 22;
+
+if(name[0] == "S" && age>=25){
+console.log("Name Starts With S and Age Is above or equal 25 ");
+}else{
+    console.log("Condition Doesn't Satisfied");
+}
+    
+//All JavaScript Identifiers Are Case Senssitive.
+if(name[0] == "s" && age>=20){
+    console.log("Name Starts With S and Age Is above 20 ");
+    }else{
+        console.log("Condition Doesn't Satisfied");
+    }
+
+
+if(name[0] == "S" && age==22){
+        console.log("Name Starts With S and Age Is 22 ");
+        }else{
+            console.log("Condition Doesn't Satisfied");
+        }
---
+++ Basic/24_OR_operator.js
@@ -0,0 +1,24 @@
+
+//In "OR" Operation, Any one or both true conditin can operation the perform. 
+let name = "Sanjida";
+let age = 22;
+
+if(name[0] == "S" || age>=25){
+console.log("Name Starts With S OR Age Is above or equal 25 ");
+}else{
+    console.log("Condition Doesn't Satisfied");
+}
+    
+//All JavaScript Identifiers Are Case Senssitive.
+if(name[0] == "s" || age>=30){
+    console.log("Name Starts With S or Age Is above 20 ");
+    }else{
+        console.log("Condition Doesn't Satisfied");
+    }
+
+
+if(name[0] == "S" || age==22){
+        console.log("Name Starts With S or Age Is 22 ");
+        }else{
+            console.log("Condition Doesn't Satisfied");
+        }
