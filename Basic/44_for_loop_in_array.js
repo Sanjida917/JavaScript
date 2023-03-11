f1553308487c8816fe7c8d2e924962f1644b5f01
Sanjida Rahman <105595349+Sanjida917@users.noreply.github.com>
2023-03-12 00:16:54 +0600

    Add files via upload

---
+++ Basic/44_for_loop_in_array.js
@@ -0,0 +1,15 @@
+
+
+let fruits = ["Mango","Banana","Jackfruit","Strawberry","Lichi","Orange","Grap"];
+
+
+console.log("Fruits Are : ");
+for(let i=0; i<fruits.length; i++){
+console.log(fruits[i]);
+}
+
+
+console.log("\nIn Upper Case Fruits Are : ");
+for(let i=0; i<=fruits.length-1; i++){
+console.log(fruits[i].toUpperCase());
+}
